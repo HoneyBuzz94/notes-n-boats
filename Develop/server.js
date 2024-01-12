a8bf20262dbbcad3829687907c99be788be8ae58
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-01-11 21:47:53 -0700

    Added in get and post requests to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const uuid = require('./helpers/uuid');
+const db = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
 
 // Static middleware pointing to the public folder
 app.use(express.static('public'));
@@ -15,6 +17,39 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// GET request
+app.get('/api/notes', (req, res) => res.json(db));
+
+// POST request
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    console.log(req.body);
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in saving note');
+    }
+  });
 
 // Listen at specified port
 app.listen(PORT, () =>
