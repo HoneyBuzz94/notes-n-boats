26108a6b2473a5ffe379c9d48bfc20fb93ca8a77
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-01-18 16:47:07 -0700

    Fixed post request to save new notes to the database.

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
 const uuid = require('./helpers/uuid');
 const db = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Static middleware pointing to the public folder
 app.use(express.static('public'));
@@ -31,13 +35,26 @@ app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
   
     // If all the required properties are present
-    if (title && text) {
+    if (req.body.title && req.body.text) {
       // Variable for the object we will save
       const newNote = {
         title,
         text,
         note_id: uuid(),
       };
+
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err){
+          console.error(err);
+        }else{
+          const parsedNotes = JSON.parse(data);
+
+          parsedNotes.push(newNote);
+
+          fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+          (writeErr) => writeErr ? console.log(writeErr) : console.log('Successfully updated notes.'));
+        };
+      });
   
       const response = {
         status: 'success',
@@ -46,7 +63,7 @@ app.post('/api/notes', (req, res) => {
   
       console.log(response);
       res.status(201).json(response);
-    } else {
+    }else{
       res.status(500).json('Error in saving note');
     }
   });
